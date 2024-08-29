b010767c8423843efe74a88ad8482d244cc9df38
Nikita-stud <nikita151998de@gmail.com>
2024-08-29 12:11:35 +0200

    carousel

--- js/api/fetchLatestPosts.js
+++ js/api/fetchLatestPosts.js
@@ -1,3 +1,55 @@
+const carousel =document.getElementById("carousel_container");
+
 export function fetchLatestPosts(posts){
 
+  const newestPosts = posts.sort((a, b) => new Date(b.date) - new Date(a.date));
+  const limitTodaysPosts = newestPosts.slice(0, 5);
+
+  const createPosts = (post) =>{
+    const anchorTag = document.createElement("a");
+
+    const div = document.createElement("div");
+    div.classList.add("post_container");
+
+    const img = document.createElement("img");
+    img.classList.add("post_img");
+    const mediaImg = post._embedded[`wp:featuredmedia`][0];
+    img.src = `${mediaImg.media_details.sizes.full.source_url}`;
+    img.alt = `${mediaImg.alt_text}`;
+
+    const title = document.createElement("h5");
+    title.classList.add("post_title")
+    title.innerText = `${post.title.rendered}`;
+
+    const p = document.createElement("p");
+    p.classList.add("post_text")
+    const tempContainer = document.createElement("div");
+    tempContainer.innerHTML = post.content.rendered;
+    const allText = tempContainer.querySelectorAll("p")
+    let paragraphs = "";
+  
+    allText.forEach(paragraph => {
+      paragraphs += paragraph.innerHTML + `<br>`;
+    });
+  
+    p.innerHTML =  paragraphs;
+
+    const readMore = document.createElement("p");
+    readMore.classList.add("post-read-more");
+    readMore.innerHTML = `Read More <i class="fa-solid fa-arrow-right"></i>`;
+     
+
+    anchorTag.append(div);
+    div.append(img);
+    div.append(title);
+    div.append(p);
+    div.append(readMore);
+
+    return anchorTag;
+  }
+
+  limitTodaysPosts.forEach(post =>{
+    carousel.appendChild(createPosts(post))
+  });
+  
 }
