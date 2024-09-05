674acef810a2a6bffcacd90095c60b00ee973285
Nikita-stud <nikita151998de@gmail.com>
2024-09-05 11:34:39 +0200

    button problems

--- js/ui/contactFormValidion.js
+++ js/ui/contactFormValidion.js
@@ -7,63 +7,63 @@ const submit = document.getElementById("cta-send");
 
 export function handleFormValidation(event){
   event.preventDefault();
+
+  name.addEventListener("change", () => {
+    const errorName = document.getElementById("name_error");
+    if(!name.value || name.value.length <= 5){
+      name.classList.add("error");
+      name.classList.remove("accept")
+      errorName.innerText= "Name must contain 5 letters";
+    }else{
+      name.classList.add("accept");
+      errorName.innerText =""
+    }
+  });
+  
+  email.addEventListener("change", () => {
+    const errorEmail = document.getElementById("email_error");
+    function validateMail(email){
+      const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+      return re.test(String(email).toLowerCase());
+    }
+    if(!email.value || !validateMail(email.value)){
+      email.classList.add("error");
+      email.classList.remove("accept")
+      errorEmail.innerHTML = `<p>Provide email in format: contact<i class="fa-solid fa-at"></i>example.com</p>`;
+    }else{
+      email.classList.add("accept");
+      errorEmail.innerText ="";
+    }
+  });
+  
+  subject.addEventListener("change", () => {
+    const errorSubject = document.getElementById("subject_error");
+    if(!subject.value || subject.value.length <= 15){
+      subject.classList.add("error");
+      subject.classList.remove("accept")
+      errorSubject.innerText = "Subject must contain at least 15 letters";
+    }else{
+      subject.classList.add("accept");
+      errorSubject.innerText="";
+    }
+  });
+  
+  message.addEventListener("change", () => {
+    const errorMessage = document.getElementById("message_error");
+    if(!message.value.length || message.value.length <=25){
+      message.classList.add("error");
+      message.classList.remove("accept")
+      errorMessage.innerText = "Message must contain 25 letters";
+    }else{
+      message.classList.add("accept");
+      errorMessage.innerText="";
+    }
+  });
+
   submit.disabled = false;
 }
 form.addEventListener("input", handleFormValidation);
-
-
-name.addEventListener("change", () => {
-  const errorName = document.getElementById("name_error");
-  if(!name.value || name.value.length <= 5){
-    name.classList.add("error");
-    name.classList.remove("accept")
-    errorName.innerText= "Name must contain 5 letters";
-  }else{
-    name.classList.add("accept");
-    errorName.innerText =""
-  }
-});
-
-email.addEventListener("change", () => {
-  const errorEmail = document.getElementById("email_error");
-  function validateMail(email){
-    const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    return re.test(String(email).toLowerCase());
-  }
-  if(!email.value || !validateMail(email.value)){
-    email.classList.add("error");
-    email.classList.remove("accept")
-    errorEmail.innerHTML = `<p>Provide email in format: contact<i class="fa-solid fa-at"></i>example.com</p>`;
-  }else{
-    email.classList.add("accept");
-    errorEmail.innerText ="";
-  }
-});
-
-
-subject.addEventListener("change", () => {
-  const errorSubject = document.getElementById("subject_error");
-  if(!subject.value || subject.value.length <= 15){
-    subject.classList.add("error");
-    subject.classList.remove("accept")
-    errorSubject.innerText = "Subject must contain at least 15 letters";
-  }else{
-    subject.classList.add("accept");
-    errorSubject.innerText="";
-  }
-});
-
-message.addEventListener("change", () => {
-  const errorMessage = document.getElementById("message_error");
-  if(!message.value.length || message.value.length <=25){
-    message.classList.add("error");
-    message.classList.remove("accept")
-    errorMessage.innerText = "Message must contain 25 letters";
-  }else{
-    message.classList.add("accept");
-    errorMessage.innerText="";
-  }
-});
+submit.disabled = true;
 
 
 
