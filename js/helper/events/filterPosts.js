77f8fab8a58030bd5be594fc0a19f035be21c97e
Nikita-stud <nikita151998de@gmail.com>
2024-09-06 15:58:12 +0200

    buttons sorted

--- js/helper/events/filterPosts.js
+++ js/helper/events/filterPosts.js
@@ -1,6 +1,11 @@
 const search = document.getElementById("search");
+const newestToOldestButton = document.getElementById("newest-to-oldest");
+const oldestToNewestButton = document.getElementById("oldest-to-newest");
 
 export function filterPosts(posts, callback){
+  const startNewestPosts = posts.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+  const startOldestPosts = posts.sort((b, a) => new Date(b.date).getTime() - new Date(a.date).getTime());
+
   
   search.addEventListener("keyup", (event) =>{
     const messageLength = event.target.value.trim().toLowerCase();
