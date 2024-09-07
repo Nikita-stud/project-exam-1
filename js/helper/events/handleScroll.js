f388888b8bca2fcee6eb7ffdab27e38464af1a8e
Nikita-stud <nikita151998de@gmail.com>
2024-09-08 00:48:06 +0200

    header color w3

--- js/helper/events/handleScroll.js
+++ js/helper/events/handleScroll.js
@@ -1,14 +1,11 @@
 const header = document.querySelector("header");
-const logoText = document.querySelector(".logo_text");
 
 export function handleScroll(){
   const scrolledY = window.scrollY;
 
   if(scrolledY > 55){
     header.classList.add("scrolled");
-    logoText.classList.add("scrolled");
   }else{
     header.classList.remove("scrolled");
-    logoText.classList.remove("scrolled");
   }
 };
