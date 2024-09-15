f868bae9895797ad84f02275861c9e2edbe4950a
Nikita-stud <nikita151998de@gmail.com>
2024-09-15 09:45:26 +0200

    php for contact

--- php/send-contact.php
+++ php/send-contact.php
@@ -5,6 +5,13 @@ $email = $_POST["email"];
 $subject = $_POST["subject"];
 $message = $_POST["message"];
 
+$to = "dev-email@wpengine.local";
+
+$messageToSend = "From $name at $email about $subject with the Inquiry: $message"
+
+if(mail($to, $subject, $messageToSend)){
+  echo "Email sent successfully";
+}
 
 
 ?>
